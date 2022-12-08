88b57047e3127f8813bb532fc517b125b1bdce6c
Elfansoer <elfansoer@gmail.com>
2022-12-08 15:06:46 +0900

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -1,1 +1,12 @@
+require( "kvparser" )
+
 print('hello, world!')
+
+-- load KV data
+local stored_data = KVParser:LoadKeyValueFromFile( "items_game.txt" )
+if not stored_data then
+  print('... failed. This might occur if "items_game.lua" is missing from the PATH folder.')
+  print('Try run update mode first.')
+  print('Aborting.')
+  return false
+end
